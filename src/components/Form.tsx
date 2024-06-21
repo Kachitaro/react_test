479b63975700c0895772169ab19b01b62b90471f
kachitaro <anhtai.dev@gmail.com>
2024-06-21 15:24:51 +0700

    handle Actions

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,19 +5,23 @@ import {
   FormProvider,
 } from "react-hook-form";
 import { ReactNode } from "react";
+import { redirect, useNavigate } from "react-router-dom";
 
 type FormProps = {
   onSubmit: SubmitHandler<FieldValues>;
   children: ReactNode;
+  redirect?: string;
 };
 
 export const Form = (props: FormProps) => {
-  const { onSubmit, children } = props;
+  const { onSubmit, children, redirect } = props;
   const methods = useForm();
   const { handleSubmit, reset } = methods;
+  const navigate = useNavigate();
 
-  const handleSubmits = (value: object) => {
+  const handleAction = (value: object) => {
     onSubmit(value);
+    if (redirect) navigate(redirect);
     reset();
   };
 
@@ -30,7 +34,7 @@ export const Form = (props: FormProps) => {
           justifyItems: "center",
           gap: "10px",
         }}
-        onSubmit={handleSubmit(handleSubmits)}>
+        onSubmit={handleSubmit(handleAction)}>
         {children}
         <button type="submit">Submit</button>
       </form>
